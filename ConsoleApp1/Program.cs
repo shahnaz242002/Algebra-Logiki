708d8b34bfe8e72b8499689a7df1cf3e04747186
shahnaz242002 <shahnazavazova@gmail.com>
2023-03-27 18:59:39 +0400

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+Console.OutputEncoding = Encoding.UTF8;
+
+Console.WriteLine("height");
+int height = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("lenght");
+int lenght = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("width");
+int width = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("mass");
+int mass = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("distance");
+int distance= Convert.ToInt32(Console.ReadLine());
+int sum = height + width + lenght;
+if (sum<150)
+{
+    Console.WriteLine("Является малогабаритным");
+}
+else if (height<100 && width< 10 && lenght< 100)
+{
+    Console.WriteLine("Является малогабаритным");
+}
+else if (mass<10)
+{
+    Console.WriteLine("Является малогабаритным");
+}
+else if (distance<3 && distance<10)
+{
+Console.WriteLine("Является малогабаритным");
+}
+else
+{
+    Console.WriteLine("Не является малогабаритным");
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using System.Security.Cryptography;
+using System.Text;
+Console.OutputEncoding = Encoding.UTF8;
+
+Console.WriteLine("name:");
+string clientName = Console.ReadLine();
+Console.WriteLine("age:");
+int age = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("count of order:");
+int kolicestvoZakazov =Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("summa potracennix deneg:");
+int summa = Convert.ToInt32(Console.ReadLine());
+if (age>60)
+{
+    Console.WriteLine("Вы получаете скидку");
+}
+else if (summa>5000 && kolicestvoZakazov > 4)
+{
+    Console.WriteLine("Вы получаете скидку");
+}
+else
+{
+    Console.WriteLine("Вы не получаете скидку");
+}
